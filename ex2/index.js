c504c1207f1325712304309ccc972ecac9dabfa5
P-Shumskiy <fwshumskiy@gmail.com>
2020-04-10 00:48:04 +0300

    refactored

--- ex2/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {Number} hours
- * @param {Number} minutes
- * @returns {Boolean}
- */
-module.exports = function (hours, minutes) {
-    return !(hours > 23 || hours < 0 || minutes < 0 || minutes > 59);
-
-};
