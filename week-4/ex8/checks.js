adb3b62e18ad7390a22138f36681428f2ac54fc1
P-Shumskiy <fwshumskiy@gmail.com>
2020-04-13 02:49:35 +0300

    week-4

--- week-4/ex8/checks.js
+++ week-4/ex8/checks.js
@@ -2,7 +2,7 @@
 var assert = require('assert');
 
 // Подключаем свою функцию
-var lib = require('./index.js');
+var lib = require('./index2.js');
 
 // Коллекция данных
 var friends = [
@@ -65,5 +65,77 @@ assert.deepEqual(result, [
     { name: 'Шерри', gender: 'Женский', email: 'danamcgee@example.com' },
     { name: 'Стелла', gender: 'Женский', email: 'waltersguzman@example.com' }
 ]);
+
+// Выполняем выборку и фильтрацию с помощью нашего конструктора
+var result = lib.query(
+  friends,
+  lib.select('name', 'gender', 'email'),
+  lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель']),
+  lib.filterIn('favoriteFruit', ['Яблоко', 'Банан']),
+);
+
+// Сравниваем полученный результат с ожидаемым
+assert.deepEqual(result, [
+    { name: 'Эмили', gender: 'Женский', email: 'example@example.com' },
+    { name: 'Мэт', gender: 'Мужской', email: 'danamcgee@example.com' },
+]);
+
+// Выполняем выборку и фильтрацию с помощью нашего конструктора
+var result = lib.query(
+  friends,
+  lib.select('name', 'gender', 'email'),
+  lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель']),
+  lib.filterIn('favoriteFruit', ['Яблоко', 'Банан']),
+  lib.select('name'),
+);
+
+// Сравниваем полученный результат с ожидаемым
+assert.deepEqual(result, [
+    { name: 'Эмили'},
+    { name: 'Мэт'}
+]);
+
+// Выполняем выборку и фильтрацию с помощью нашего конструктора
+var result = lib.query(
+  friends
+);
+
+// Сравниваем полученный результат с ожидаемым
+assert.deepEqual(result,
+  friends
+);
+
+// Выполняем выборку и фильтрацию с помощью нашего конструктора
+var result = lib.query(
+  friends,
+  lib.select('name', 'gender', 'email'),
+  lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель']),
+  lib.select('favoriteFruit')
+);
+
+// Сравниваем полученный результат с ожидаемым
+assert.deepEqual(result, []);
+
+// Выполняем выборку и фильтрацию с помощью нашего конструктора
+var result = lib.query(
+  friends,
+  lib.select('name', 'gender', 'email'),
+  lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель']),
+  lib.filterIn('favoriteFruit', ['Банан'])
+);
+
+// Сравниваем полученный результат с ожидаемым
+assert.deepEqual(result, []);
+
+// Выполняем выборку и фильтрацию с помощью нашего конструктора
+var result = lib.query(
+  friends,
+  lib.select('name', 'gender', 'email'),
+  lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель']),
+  lib.filterIn('favoriteFruit', ['Фейхоа'])
+);
+
+// Сравниваем полученный результат с ожидаемым
+assert.deepEqual(result, []);
 
 console.info('OK!');
--- week-4/ex8/index.js
+++ week-4/ex8/index.js
@@ -1,5 +1,9 @@
-function query(collection, fields, filterIn) {
-    if (fields && filterIn) {
+function query(collection) {
+    if (arguments.length === 1) {
+        return collection
+    } else {
+        const {fields, filterIn} = formatInput(arguments)
+
         const {propertyToFilter, valuesToFilter} = filterIn;
 
         const filteredCollection = collection.filter((obj) => {
@@ -7,28 +11,63 @@ function query(collection, fields, filterIn) {
         });
 
         const selectedCollection = [];
-        filteredCollection.forEach((value, index) => {
+        filteredCollection.forEach((value) => {
             const newObj = {};
             Object.keys(value).map((field) => {
                 if (fields.includes(field)) {
                     newObj[field] = value[field]
                 }
             });
-            selectedCollection.push(newObj)
+            if (Object.keys(newObj).length){
+                selectedCollection.push(newObj)
+            }
         });
         return selectedCollection
-    } else {
-        return collection
     }
 }
 
 function select() {
-    return [...arguments]
+    return {
+        args: [...arguments],
+        type: 'select'
+    }
 }
 
 function filterIn(property, values) {
-    return {propertyToFilter: property,
-        valuesToFilter: [...values]}
+    return {
+        propertyToFilter: property,
+        valuesToFilter: [...values],
+        type: 'filter'
+    }
+}
+
+function intersect(args) {
+    return [...args].reduce(
+      (accumulator, current) => accumulator.filter(
+        (x) => current.includes(x))
+    )
+}
+
+function formatInput(args) {
+    const selectionsToIntersect = []
+    const filtersToIntersect = []
+    let propertyToFilter
+
+    for (let i = 1; i < args.length; i++) {
+        let obj = args[i]
+        if (obj.type === 'select') {
+            selectionsToIntersect.push(obj.args)
+        } else if (obj.type === 'filter') {
+            filtersToIntersect.push(obj.valuesToFilter)
+            propertyToFilter = obj.propertyToFilter
+        }
+    }
+    const fields = intersect(selectionsToIntersect)
+    const filterIn = {
+        propertyToFilter,
+        valuesToFilter: intersect(filtersToIntersect)
+    }
+    return {fields, filterIn}
 }
 
 module.exports = {
